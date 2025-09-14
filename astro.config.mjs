20ef1067fefa568ccfae86ad0106142b2b9fcdf2
atgif <43119959+atgif@users.noreply.github.com>
2025-09-14 18:51:43 -0500

    Update astro.config.mjs removed import cloudflare from '@astrojs/cloudflare';
    import cloudflare from '@astrojs/cloudflare'; 
    to
    import staticAdapter from '@astrojs/adapter-static';

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
+import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  adapter: staticAdapter()
 });
