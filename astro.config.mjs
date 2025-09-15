93ca90d1df0e8138ac29ceaf12400059a56f32ac
atgif <43119959+atgif@users.noreply.github.com>
2025-09-14 20:25:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-export default defineConfig({
-  integrations: [svelte()],
-});
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://theoglaw.com",
-  integrations: [mdx(), sitemap()]
+  integrations: [svelte(), mdx(), sitemap()]
 });
