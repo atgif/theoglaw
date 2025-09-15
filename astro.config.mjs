5c686a72d8155ff513795f4b96f4698090b3a834
atgif <43119959+atgif@users.noreply.github.com>
2025-09-14 20:17:59 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+export default defineConfig({
+  integrations: [svelte()],
+});
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
