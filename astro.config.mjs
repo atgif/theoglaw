6c36f36615a0a6d906434bc469c6fad6d48a63ed
atgif <43119959+atgif@users.noreply.github.com>
2025-09-14 19:10:21 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  adapter: staticAdapter()
+  adapter: staticAdapter(),
+  site: "https://theoglaw.com",
+  integrations: [mdx(), sitemap()]
 });
